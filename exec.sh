204b2aa9a318641f9de032eeda3b5dabe05ad0ca
Alexandre Gracia i Calvo <alexusgracia@github.com>
2025-10-01 13:41:19 +0200

    refactor main

--- exec.sh
+++ exec.sh
@@ -1,32 +1,16 @@
-#!/bin/bash
-set -e  # si hay un error, se detiene la ejecución
+module load python/3.10.2 COMPSs
 
-echo "🔹 Eliminando entorno previo (si existe)..."
-pycompss env remove docker-compss || true
+#export PYTHONPATH=${PYTHONPATH}:$(pwd)/src:$(pwd):$(pwd)/stability_analysis
+export PYTHONPATH=/gpfs/projects/bsc19/upc848455/packages:$(pwd)/src:$(pwd):$(pwd)/stability_analysis
+enqueue_compss \
+  --pythonpath="${PYTHONPATH}" \
+  --lang=python \
+  --project_name=bsc19 \
+  --qos=debug \
+  --num_nodes=1 \
+  --job_execution_dir="$(pwd)" \
+  --log_dir="$(pwd)" \
+  --worker_working_dir="$(pwd)" \
+  --master_working_dir="$(pwd)" \
+  /home/upc/upc848455/probabilistic_contingencies/main_parallel.py
 
-echo "🔹 Inicializando nuevo entorno docker-compss con Docker..."
-pycompss init -n docker-compss docker -i compss/compss:3.3.3
-
-echo "🔹 Cambiando al entorno docker-compss..."
-pycompss env change docker-compss
-
-#docker exec -it pycompss-master-docker-compss ls -l
-
-# Copiar el contenido de stability analysis al contenedor de docker
-docker cp stability_analysis/ pycompss-master-docker-compss:/Dades/git/probabilistic_contingencies/  # esto lo copia en la carpeta home dentro de tu contenedor
-docker exec -it pycompss-master-docker-compss pip install -e /Dades/git/probabilistic_contingencies/stability_analysis
-
-docker exec -it pycompss-master-docker-compss pip install -r /Dades/git/probabilistic_contingencies/requirements.txt
-
-pycompss run main_parallel.py
-
-#docker exec -it pycompss-master-docker-compss bash
-
-#docker exec -it pycompss-master-docker-compss pip install numpy
-
-
-#echo "🔹 Ejecutando aplicación con PyCOMPSs..."
-#pycompss run main_parallel.py
-
-# Copiar archivo al exterior
-#docker cp pycompss-master-docker-compss:Dades/git/Dades/git/probabilistic_contingencies/results_parallel.json ./results_parallel.json
